467daf73817cad51fc3063627fddd624d524a2ad
Denis Chaplygin <denis.chaplygin@wolt.com>
2020-01-22 18:05:38 +0200

    Looks like compressed header is not supported by everyone

--- src/main/java/com/wolt/osm/parallelpbf/blob/BlobWriter.java
+++ src/main/java/com/wolt/osm/parallelpbf/blob/BlobWriter.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.zip.Deflater;
 
 @RequiredArgsConstructor
@@ -23,24 +24,34 @@ public class BlobWriter {
     }
 
     public boolean write(byte[] blob, String type) {
-        if (!BlobInformation.TYPE_OSM_DATA.equals(type) && !BlobInformation.TYPE_OSM_HEADER.equals(type)) {
+        boolean compress;
+        if (BlobInformation.TYPE_OSM_DATA.equals(type)) {
+            compress = true;
+        } else if (BlobInformation.TYPE_OSM_HEADER.equals(type)) {
+            compress = false;
+        } else {
             log.error("Unsupported Blob type: {}", type);
             return false;
         }
 
-        // Compress blob
-        Deflater compresser = new Deflater(Deflater.BEST_COMPRESSION);
-        byte[] compressedBlob = new byte[blob.length];
-        compresser.setInput(blob);
-        compresser.finish();
-        int compressedBlobLength = compresser.deflate(compressedBlob);
-        compresser.end();
-
         // Form DataBlob
-        byte[] dataBlob = Fileformat.Blob.newBuilder()
-                .setRawSize(blob.length)
-                .setZlibData(ByteString.copyFrom(compressedBlob, 0, compressedBlobLength))
-                .build().toByteArray();
+        byte[] dataBlob;
+        if (compress) {
+            Deflater compressor = new Deflater(Deflater.BEST_COMPRESSION);
+            byte[] compressedBlob = new byte[blob.length];
+            compressor.setInput(blob);
+            compressor.finish();
+            int compressedBlobLength = compressor.deflate(compressedBlob);
+            compressor.end();
+            dataBlob = Fileformat.Blob.newBuilder()
+                    .setRawSize(blob.length)
+                    .setZlibData(ByteString.copyFrom(compressedBlob, 0 ,compressedBlobLength))
+                    .build().toByteArray();
+        } else {
+            dataBlob = Fileformat.Blob.newBuilder()
+                    .setRaw(ByteString.copyFrom(blob))
+                    .build().toByteArray();
+        }
 
         // Form headerBlob
         byte[] headerBlob = Fileformat.BlobHeader.newBuilder()
