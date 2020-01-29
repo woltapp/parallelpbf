4b2f1733da26d12c686c6aee9922eeda0a2b9b6c
Denis Chaplygin <denis.chaplygin@wolt.com>
2020-01-29 13:31:44 +0200

    Removed code duplications in integration tests

--- src/test/java/com/wolt/osm/parallelpbf/ParallelBinaryParserIT.java
+++ src/test/java/com/wolt/osm/parallelpbf/ParallelBinaryParserIT.java
@@ -21,10 +21,11 @@ import com.wolt.osm.parallelpbf.entity.Node;
 import com.wolt.osm.parallelpbf.entity.Relation;
 import com.wolt.osm.parallelpbf.entity.RelationMember;
 import com.wolt.osm.parallelpbf.entity.Way;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.InputStream;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Optional;
 import java.util.function.Consumer;
@@ -32,6 +33,7 @@ import java.util.function.Consumer;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParallelBinaryParserIT {
+    /* Reader specific part */
     private Node simpleNode;
     private Node taggedNode;
 
@@ -60,28 +62,42 @@ class ParallelBinaryParserIT {
         }
     };
 
-    @Test
-    void testParser() {
-        InputStream input = Thread.currentThread().getContextClassLoader()
-                .getResourceAsStream("sample.pbf");
+    /* Writer specific part */
+    private ParallelBinaryWriter writer;
 
+    private void writeNodes(Node node) {
+        writer.write(node);
+    }
 
+    private void writeWays(Way way) {
+        writer.write(way);
+    }
+
+    private void writeRelations(Relation relation) {
+        writer.write(relation);
+    }
+
+    @SneakyThrows
+    private void closeOnComplete() {
+        writer.close();
+    }
+
+    /* Shared code */
+    private void parse(InputStream input) {
         new ParallelBinaryParser(input, 1)
                 .onNode(nodeChecker)
                 .onWay(wayChecker)
                 .onRelation(relationsChecker)
                 .parse();
+    }
 
+    private void testSimpleNode() {
         assertEquals(51.7636027, simpleNode.getLat(), 0.0000001);
         assertEquals(-0.22875700000000002, simpleNode.getLon(), 0.0000001);
         assertTrue(simpleNode.getTags().isEmpty());
-        assertEquals(234999, simpleNode.getInfo().getUid());
-        assertEquals("Nicholas Shanks", simpleNode.getInfo().getUsername());
-        assertEquals(1, simpleNode.getInfo().getVersion());
-        assertEquals(1267144226000L, simpleNode.getInfo().getTimestamp());
-        assertEquals(3977001, simpleNode.getInfo().getChangeset());
-        assertTrue(simpleNode.getInfo().isVisible());
+    }
 
+    private void testTaggedNode() {
         assertEquals(51.76511770000001, taggedNode.getLat(), 0.0000001);
         assertEquals(-0.23366680000000006, taggedNode.getLon(), 0.0000001);
         assertFalse(taggedNode.getTags().isEmpty());
@@ -89,6 +105,40 @@ class ParallelBinaryParserIT {
         assertEquals("Oaktree Close", taggedNode.getTags().get("name"));
         assertTrue(taggedNode.getTags().containsKey("highway"));
         assertEquals("bus_stop", taggedNode.getTags().get("highway"));
+    }
+
+    private void testWay() {
+        assertEquals(Arrays.asList(1709246789L, 1709246746L, 1709246741L, 1709246791L), taggedWay.getNodes());
+        assertFalse(taggedWay.getTags().isEmpty());
+        assertTrue(taggedWay.getTags().containsKey("highway"));
+        assertEquals("footway", taggedWay.getTags().get("highway"));
+    }
+
+    public void testRelation() {
+        Optional<RelationMember> member = taggedRelation.getMembers().stream().filter(m -> m.getId() == 25896432).findAny();
+        assertTrue(member.isPresent());
+        assertEquals("forward", member.get().getRole());
+        Assertions.assertEquals(RelationMember.Type.WAY, member.get().getType());
+        assertFalse(taggedRelation.getTags().isEmpty());
+        assertTrue(taggedRelation.getTags().containsKey("route"));
+        assertEquals("bicycle", taggedRelation.getTags().get("route"));
+    }
+
+    @Test
+    void testParser() {
+        InputStream input = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("sample.pbf");
+        parse(input);
+
+        testSimpleNode();
+        assertEquals(234999, simpleNode.getInfo().getUid());
+        assertEquals("Nicholas Shanks", simpleNode.getInfo().getUsername());
+        assertEquals(1, simpleNode.getInfo().getVersion());
+        assertEquals(1267144226000L, simpleNode.getInfo().getTimestamp());
+        assertEquals(3977001, simpleNode.getInfo().getChangeset());
+        assertTrue(simpleNode.getInfo().isVisible());
+
+        testTaggedNode();
         assertEquals(104459, taggedNode.getInfo().getUid());
         assertEquals("NaPTAN", taggedNode.getInfo().getUsername());
         assertEquals(1, taggedNode.getInfo().getVersion());
@@ -97,10 +147,7 @@ class ParallelBinaryParserIT {
         assertTrue(taggedNode.getInfo().isVisible());
 
 
-        assertEquals(Arrays.asList(1709246789L, 1709246746L, 1709246741L, 1709246791L), taggedWay.getNodes());
-        assertFalse(taggedWay.getTags().isEmpty());
-        assertTrue(taggedWay.getTags().containsKey("highway"));
-        assertEquals("footway", taggedWay.getTags().get("highway"));
+        testWay();
         assertEquals(470302, taggedWay.getInfo().getUid());
         assertEquals("Kjc", taggedWay.getInfo().getUsername());
         assertEquals(1, taggedWay.getInfo().getVersion());
@@ -108,13 +155,7 @@ class ParallelBinaryParserIT {
         assertEquals(11245909, taggedWay.getInfo().getChangeset());
         assertFalse(taggedWay.getInfo().isVisible());
 
-        Optional<RelationMember> member = taggedRelation.getMembers().stream().filter(m -> m.getId() == 25896432).findAny();
-        assertTrue(member.isPresent());
-        assertEquals("forward", member.get().getRole());
-        Assertions.assertEquals(RelationMember.Type.WAY, member.get().getType());
-        assertFalse(taggedRelation.getTags().isEmpty());
-        assertTrue(taggedRelation.getTags().containsKey("route"));
-        assertEquals("bicycle", taggedRelation.getTags().get("route"));
+        testRelation();
         assertEquals(24119, taggedRelation.getInfo().getUid());
         assertEquals("Mauls", taggedRelation.getInfo().getUsername());
         assertEquals(81, taggedRelation.getInfo().getVersion());
@@ -122,4 +163,36 @@ class ParallelBinaryParserIT {
         assertEquals(11640673, taggedRelation.getInfo().getChangeset());
         assertFalse(taggedRelation.getInfo().isVisible());
     }
+
+    @Test
+    void testWriter() throws IOException {
+        String outputFilename = System.getProperty("java.io.tmpdir")+"/parallel.pbf";
+        File outputFile = new File(outputFilename);
+        if (outputFile.exists()) {
+            outputFile.delete();
+        }
+        outputFile.createNewFile();
+        OutputStream output = new FileOutputStream(outputFile);
+
+        writer = new ParallelBinaryWriter(output,1, null);
+        writer.start();
+
+        InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream("sample.pbf");
+        new ParallelBinaryParser(input, 1)
+                .onComplete(this::closeOnComplete)
+                .onNode(this::writeNodes)
+                .onWay(this::writeWays)
+                .onRelation(this::writeRelations)
+                .parse();
+        output.close();
+
+        InputStream written = new FileInputStream(outputFile);
+        parse(written);
+
+        testSimpleNode();
+        testTaggedNode();
+        testWay();
+        testParser();
+    }
+
 }
